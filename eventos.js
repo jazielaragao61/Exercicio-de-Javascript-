45d1ce9d3c4e2752586948dff944d69a7a1b205e
Jaziel <jazievi@hotmail.com>
2021-11-01 21:38:35 -0300

    v1

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
-var frmNome = nome.value
+var frmNome = nome.value //document.getElementById('nome').valu
 var frmEmail = email.value
 var frmSenha = senha.value
 
 //Botão cadastrar add evento
 
-btnCadastrar.event
+btnCadastrar.addEventListener('click', function() {
+    alert('click no botão cadastrado')
+})
+
+nome.addEventListener('blur', function(){
+    alert(frmNome)
+})
