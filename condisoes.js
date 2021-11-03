cb105619d7f7c41c1ea1ff9b42a02613a2b87ff3
Jaziel <jazievi@hotmail.com>
2021-11-03 20:32:43 -0300

    1

---
+++ condisoes.js
@@ -0,0 +1,20 @@
+var idade = parseInt(prompt('Digite a sua idade:'))
+
+if (idade >= 18){
+    alert()
+} else {
+    alert()
+    window.location - "https://google.com";
+}
+
+var hora = parseInt(prompt("Digite a hora: "))
+var respostaHora
+
+if (hora < 12 ) {
+    respostaHora = "Bom dia !"
+} else if (hora < 18) {
+    respostaHora = "Boa tarde"
+} else {
+    respostaHora = "Boa noite!"
+}
+alert(respostaHora)
